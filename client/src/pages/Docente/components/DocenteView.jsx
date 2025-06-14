ff3b347b2afc7c27fa1bb900de731bb413631f60
royandresdev <royhuamanavila@gmail.com>
2025-06-13 21:01:33 -0500

    feat: agregar hook useDocente y actualizar componentes relacionados

--- client/src/pages/Docente/components/DocenteView.jsx
+++ client/src/pages/Docente/components/DocenteView.jsx
@@ -1,8 +1,8 @@
 import StudentGradesContainer from "../containers/StudentGradesContainer";
 import GradeFilter from "./GradeFilter";
 
-const DocenteView = ({ docente }) => {
-  const { user, teacherId, subject } = docente
+const DocenteView = ({ user }) => {
+  const { fullName, profileImageUrl, userId, subjects } = user
 
   return (
     <div id="docente" className="max-w-[1216px] m-auto">
@@ -11,13 +11,13 @@ const DocenteView = ({ docente }) => {
         {/* Card izquierda */}
         <div className="card border border-brand-primary w-full flex">
           { /* Foto de perfil del docente */}
-          <img className="max-h-[174px] max-w-[200px]" src={user?.profileImageUrl} alt="Perfil Docente" />
+          <img className="max-h-[174px] max-w-[200px]" src={profileImageUrl} alt="Perfil Docente" />
 
           {/* Datos personales */}
           <div className="p-10">
             <p className="font-bold text-grey-1 text-h5 leading-4">Docente</p>
-            <p className="font-bold text-h4 text-brand-primary">{user?.fullName}</p>
-            <p className="font-bold text-grey-1">ID <span className="text-brand-primary">{teacherId}</span></p>
+            <p className="font-bold text-h4 text-brand-primary">{fullName}</p>
+            <p className="font-bold text-grey-1">ID <span className="text-brand-primary">{userId}</span></p>
           </div>
         </div>
 
@@ -41,7 +41,7 @@ const DocenteView = ({ docente }) => {
       {/* Search Bar */}
       <div className="space-y-6 mb-6">
         <h2 className="text-h3">Califique y Retroalimente a sus Estudiantes</h2>
-        <GradeFilter subjects={[subject]} />
+        <GradeFilter subjects={subjects} />
         <h3 className="text-h5 text-grey-1">2024-3 / Educación Física / 7A Bachillerato</h3>
       </div>
 
