64275694db65f5d50accaad8b22811a577e7d9f0
royandresdev <royhuamanavila@gmail.com>
2025-12-30 20:58:04 -0500

    feat: convertir el contenedor del logo en un enlace y mejorar la accesibilidad 🔗✨

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="max-w-7xl mx-auto py-6 px-6 md:px-12">
       <div className="max-w-7xl mx-auto">
-        <div className="flex items-center gap-2">
+        <a href="/" className="flex items-center gap-2">
           <div className="w-8 h-8 flex items-center justify-center">
             <img src="/Logo.svg" alt="EduTrack Logo" />
           </div>
@@ -10,7 +10,7 @@ const Header = () => {
             <span className="text-purple-900 font-bold">Edu</span>
             <span className="text-gray-400">Track</span>
           </h1>
-        </div>
+        </a>
       </div>
     </header>
   );
